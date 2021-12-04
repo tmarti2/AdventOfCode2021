3b9075c9bc97d4829153e6662f04af783d309824
Thibault Martin <twenty2bar+github@gmail.com>
2021-12-04 23:17:35 +0100

    Cleaning day4...

--- day4/day4.ml
+++ day4/day4.ml
@@ -1,153 +1,116 @@
-type cell = Notinit | Notmarked of int | Marked of int
+open Base
 
 type board = {
-  grid : cell Array.t Array.t;
+  grid : (bool*int) Array.t Array.t;
   mutable win : bool;
 }
 
-let get_first_Notinit arr =
-  let rec aux i =
-    if i < Array.length arr then begin
-      if Array.get arr i = Notinit
-      then i
-      else aux (i+1)
-    end
-    else assert false
-  in
-  aux 0
+let to_int_arr s =
+  String.split ~on:' ' s
+  |> List.filter ~f:(fun s -> not @@ String.equal "" s)
+  |> List.map ~f:(fun s -> (false, Int.of_string s))
+  |> List.to_array
 
 let parse_file data =
-  let rec aux todo current_grid numbers grids =
-    match todo with
-    | [] ->
-      (match current_grid with
-       | None -> numbers,grids
-       | Some grid -> numbers,({grid;win=false}::grids))
-    | hd :: tail ->
-      if String.contains hd ',' then
-        aux tail current_grid (List.map int_of_string @@ String.split_on_char ',' hd) grids
-      else begin
-        try
-          if String.equal hd "" then
-            match current_grid with
-            | None -> aux tail None numbers grids
-            | Some grid -> aux tail None numbers ({grid;win=false}::grids)
-          else begin
-            Scanf.sscanf (String.trim hd) "%d %d %d %d %d" (fun a b c d e ->
-                match current_grid with
-                | None ->
-                  let curr = Array.init 5 (fun _ -> Array.make 5 Notinit) in
-                  List.iteri (fun i v ->
-                      Array.set (Array.get curr i) 0 (Notmarked v)
-                    ) [a;b;c;d;e];
-                  aux tail (Some curr) numbers grids
-                | Some grid ->
-                  let first = get_first_Notinit (Array.get grid 0) in
-                  List.iteri (fun i v ->
-                      Array.set (Array.get grid i) first (Notmarked v)
-                    ) [a;b;c;d;e];
-                  aux tail (Some grid) numbers grids
-              )
-          end
-        with
-        | _ -> aux tail current_grid numbers grids
-      end
-  in
-  aux data None [] []
+  match data with
+  | [] -> failwith "empty file"
+  | nbs :: grids ->
+    let numbers = String.split ~on:',' nbs |> List.map ~f:Int.of_string in
+    let rec aux to_read grids =
+      match to_read with
+      | [] -> numbers, grids
+      | "" :: rest -> aux rest grids
+      | l1 :: l2 :: l3 :: l4 :: l5 :: rest ->
+        let arr_lines = List.map ~f:to_int_arr [l1;l2;l3;l4;l5] in
+        let grid = List.to_array arr_lines |> Array.transpose_exn in
+        aux rest ({grid;win=false}::grids)
+      | _ -> failwith "wrong file format"
+    in
+    aux grids []
 
 let mark_number_grid v b =
   let pos = ref None in
-  Array.iteri ( fun x c ->
-      Array.iteri ( fun y n ->
-          match n with
-          | Notmarked n when n = v ->
-            Array.set c y (Marked n);
-            pos := Some (x,y)
-          | _ -> ()
+  Array.iteri ~f:(fun x c ->
+      Array.iteri ~f:(fun y (status,n) ->
+          if not status && n = v
+          then begin
+            pos := Some (x,y);
+            c.(y) <- (true, n)
+          end
         ) c
     ) b.grid;
-  !pos,b
+  !pos
 
-let is_bingo_r (_,y) g =
-  let exception NotBingo in
-  try
-    Array.iter (fun c ->
-        match Array.get c y with
-        | Notmarked _ -> raise NotBingo
-        | _ -> ()
-      ) g;
-    true
-  with
-    NotBingo -> false
+let is_bingo_col x g =
+  not @@ Array.exists ~f:(fun (status,_) -> not status) g.(x)
 
-let is_bingo_c (x,_) g =
-  let exception NotBingo in
-  let c = Array.get g x in
-  try
-    Array.iter (fun v ->
-        match v with
-        | Notmarked _ -> raise NotBingo
-        | _ -> ()
-      ) c;
-    true
-  with
-    NotBingo -> false
+let is_bingo_row y g =
+  Array.transpose_exn g |> is_bingo_col y
 
 let compute_score nb g =
-  let cpt = ref 0 in
-  Array.iter (fun c ->
-      Array.iter (fun v ->
-          match v with
-          | Notmarked x -> cpt := !cpt + x
-          | _ -> ()
-        ) c
-
-    ) g;
-  !cpt * nb
+  Array.fold g ~init:0 ~f:(fun n c ->
+      Array.fold c ~init:n ~f:(fun n' (status,v) ->
+          if not status then n'+v else n'
+        )
+    ) * nb
 
 let is_winner pos g =
   match pos with
   | Some (x,y) ->
-    is_bingo_c (x,y) g || is_bingo_r (x,y) g
+    is_bingo_col x g || is_bingo_row y g
   | _ -> false
 
-let solv1 nbs (grids:board list) =
+let solv1 nbs grids =
   let rec aux todo =
     match todo with
     | [] -> failwith "No win"
     | n :: tail ->
-      let res = List.map (fun b -> mark_number_grid n b) grids in
       let winner =
-        List.find_opt (fun (pos,b) -> is_winner pos b.grid) res
+        List.map ~f:(fun b -> mark_number_grid n b) grids |>
+        List.findi ~f:(fun i pos ->
+            is_winner pos (List.nth_exn grids i).grid
+          )
       in
       match winner with
       | None -> aux tail
-      | Some (_,g) -> Printf.printf "Part 1 : %d\n" (compute_score n g.grid)
+      | Some (i,_) ->
+        Stdio.printf "Part 1 : %d\n"
+          (compute_score n (List.nth_exn grids i).grid)
   in aux nbs
-
 
 let solv2 nbs grids =
   let rec aux todo grids last =
     match todo with
-    | [] -> Printf.printf "Part 2 : %d\n" last
+    | [] -> Stdio.printf "Part 2 : %d\n" last
     | n :: tail ->
-      let res = List.map (fun b -> mark_number_grid n b) grids in
       let winners =
-        List.find_all (fun (pos,b) -> is_winner pos b.grid) res
+        let cells = List.map ~f:(fun b -> mark_number_grid n b) grids in
+        List.filteri ~f:(fun i b ->
+            is_winner (List.nth_exn cells i) b.grid
+          ) grids
       in
       match winners with
       | [] -> aux tail grids last
-      | (_,hd)::_ ->
-        List.iter (fun (_,b) -> b.win <- true) winners;
-        aux tail (List.filter (fun b -> not b.win) grids) (compute_score n hd.grid)
+      | hd::_ ->
+        List.iter ~f:(fun b -> b.win <- true) winners;
+        aux tail (List.filter ~f:(fun b -> not b.win) grids) (compute_score n hd.grid)
   in aux nbs grids 0
 
+let reset_board b =
+  Array.iter ~f:(fun c ->
+      Array.iteri ~f:(fun y (_,i) ->
+          c.(y) <- (false,i)
+        ) c;
+    ) b.grid
+
+let reset_boards boards =
+  List.iter ~f:reset_board boards
 
 let main file =
-  let nbs,grids = Arg.read_arg file |> Array.to_list |> parse_file in
-  Printf.printf "%d tirages, %d grilles\n" (List.length nbs) (List.length grids);
+  let nbs,grids = Stdio.In_channel.read_lines file |> parse_file in
+  Stdio.printf "%d tirages, %d grilles\n" (List.length nbs) (List.length grids);
   solv1 nbs grids;
+  reset_boards grids;
   solv2 nbs grids
-
 
 let _ = main "example.txt"; main "input.txt"
