edf296d57715955f0e79abca5850689f020bbc45
Thibault Martin <twenty2bar+github@gmail.com>
2021-12-05 14:03:26 +0100

    Merci virgule

--- day5/day5.ml
+++ day5/day5.ml
@@ -6,54 +6,35 @@ let max v1 v2 v3 =
   else v3
 
 let parse_file data =
+  let sign_delta i = match Int.sign i with Neg -> -1 | Zero -> 0 | Pos -> 1 in
   let parsed_data,mx,my =
     List.fold_left ~init:([],0,0) ~f:(fun (parsed,mx,my) line ->
         Caml.Scanf.sscanf line "%d,%d -> %d,%d" (fun x1 y1 x2 y2 ->
-            ((x1,y1),(x2,y2))::parsed,(max mx x1 x2),(max my y1 y2)
+            let delta = sign_delta (x2 - x1), sign_delta (y2 - y1) in
+            ((x1,y1),(x2,y2),delta)::parsed,
+            (max mx x1 x2), (max my y1 y2)
           )
       ) data
   in
   List.rev parsed_data,mx+1,my+1
 
-let fill_col map x y1 y2 cpt =
-  let f,t = if y1 <= y2 then y1,y2 else y2,y1 in
-  Array.foldi ~init:cpt ~f:(fun y n v ->
-      if y >= f && y <= t
-      then (map.(x).(y) <- v + 1; if v + 1 = 2 then n+1 else n)
-      else n
-    ) map.(x)
+let is_diag dx dy = dx <> 0 && dy <> 0
 
-let fill_row map y x1 x2 cpt =
-  let f,t = if x1 <= x2 then x1,x2 else x2,x1 in
-  Array.foldi ~init:cpt ~f:(fun x n c ->
-      if x >= f && x <= t
-      then (c.(y) <- c.(y) + 1; if c.(y) = 2 then n + 1 else n)
-      else n
-    ) map
-
-let fill_diag map (x1,y1) (x2,y2) cpt =
-  let offset_x = if x1 > x2 then -1 else 1 in
-  let offset_y = if y1 > y2 then -1 else 1 in
-  let rec next x y n =
-    map.(x).(y) <- map.(x).(y) + 1;
-    let n = if map.(x).(y) = 2 then n + 1 else n in
-    if x = x2 && y = y2
-    then n
-    else next (x + offset_x) (y + offset_y) n
-  in
-  next x1 y1 cpt
+let fill_line ~diag map ((x1,y1),(x2,y2),(dx,dy)) cpt =
+  if (diag && is_diag dx dy) || (not diag && not (is_diag dx dy)) then
+    let rec next x y n =
+      map.(x).(y) <- map.(x).(y) + 1;
+      let n = if map.(x).(y) = 2 then n + 1 else n in
+      if x = x2 && y = y2
+      then n
+      else next (x + dx) (y + dy) n
+    in
+    next x1 y1 cpt
+  else cpt
 
 let solve ~diag ?(cpt=0) map data =
-  List.fold ~init:cpt ~f:(fun n ((x1,y1),(x2,y2)) ->
-      if diag && x1 <> x2 && y1 <> y2 then
-        fill_diag map (x1,y1) (x2,y2) n
-      else begin
-        if x1 = x2 && not diag then
-          fill_col map x1 y1 y2 n
-        else if y1 = y2 && not diag then
-          fill_row map y1 x1 x2 n
-        else n
-      end
+  List.fold ~init:cpt ~f:(fun n line ->
+      fill_line ~diag map line n
     ) data
 
 let main file =
@@ -62,6 +43,5 @@ let main file =
   let res1 = solve ~diag:false map data in
   let res2 = solve ~diag:true ~cpt:res1 map data in
   Stdio.printf "Part 1 : %d\npart 2 : %d\n" res1 res2
-
 
 let _ = main "example.txt"; main "input.txt"
