12534917a335bbfe4ba329affa7d7f92a68401a2
Thibault Martin <twenty2bar+github@gmail.com>
2021-12-06 16:34:13 +0100

    Pas forcément opti mais plus joli

--- day6/day6.ml
+++ day6/day6.ml
@@ -1,30 +1,24 @@
 open Base
 
 let parse_file data =
-  match data with
-  | [] -> failwith "Empty file"
-  | [l] ->
-    let arr = Array.create ~len:9 0 in
-    String.split ~on:',' l |> List.map ~f:Int.of_string
-    |> List.iter ~f:(fun v -> arr.(v) <- arr.(v) + 1);
-    Array.to_list arr
-  | _ -> assert false
+  let l = List.hd_exn data |> String.split ~on:',' |> List.map ~f:Int.of_string in
+  List.init 9 ~f:(fun i -> List.count l ~f:((=) i))
 
 let solve days start =
-  let rec step days_left current =
-    match current with
-    | _ when days_left = 0 -> current
-    | j0::j1::j2::j3::j4::j5::j6::j7::j8::[] ->
-      step (days_left-1) (j1::j2::j3::j4::j5::j6::j0+j7::j8::j0::[])
+  let rec step n l =
+    match n,l with
+    | 0, _-> l
+    | _, [j0;j1;j2;j3;j4;j5;j6;j7;j8] ->
+      step (n-1) [j1;j2;j3;j4;j5;j6;j0+j7;j8;j0]
     | _ -> assert false
   in
   step days start
-  |> List.fold_left ~init:0 ~f:(fun acc i -> acc+i)
 
 let main file =
+  let count = List.reduce_exn ~f:(+) in
   let start = Stdio.In_channel.read_lines file |> parse_file in
   let res1 = solve 80 start in
-  let res2 = solve 256 start in
-  Stdio.printf "Part 1 : %d\nPart 2 : %d\n" res1 res2
+  let res2 = solve 176 res1 in
+  Stdio.printf "Part 1 : %d\nPart 2 : %d\n" (count res1) (count res2)
 
 let _ = main "example.txt"; main "input.txt"
