1da917a5b217bc57db25ea7aa275cec71f300807
Thibault Martin <twenty2bar+github@gmail.com>
2021-12-07 16:55:00 +0100

    use round ?dir

--- day7/day7.ml
+++ day7/day7.ml
@@ -7,11 +7,10 @@ let median data =
   let arr = List.sort ~compare data |> List.to_array in
   (arr.((Array.length arr - 1) / 2) + arr.(Array.length arr / 2)) / 2
 
-let average data =
+let average ?dir data =
   Float.of_int @@ List.length data
   |> (/.) (List.map ~f:Float.of_int data |> List.sum (module Float) ~f:Fn.id)
-  |> (fun mean -> Float.round_down mean, Float.round_up mean)
-  |> (fun (down,up) -> Float.to_int down, Float.to_int up)
+  |> Float.round ?dir |> Float.to_int
 
 let solve v f data =
   List.sum (module Int) ~f:(fun v' -> f @@ abs (v - v')) data
@@ -19,8 +18,8 @@ let solve v f data =
 let solve1 data = solve (median data) Fn.id data
 
 let solve2 data =
-  let down,up = average data in
   let f = (fun n -> n * (n + 1) / 2) in
+  let down,up = average ~dir:`Down data, average ~dir:`Up data in
   min (solve down f data) (solve up f data)
 
   let main file =
