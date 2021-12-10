33f66590a6e9920a0d7e08ba2e0c55ff868d6270
Thibault Martin <twenty2bar+github@gmail.com>
2021-12-10 17:41:55 +0100

    clean day 10

--- day10/day10.ml
+++ day10/day10.ml
@@ -28,13 +28,10 @@ let partition_line line =
     | (('(' | '[' | '{' | '<') as c) :: rest ->
       Stack.push s (find_closing c); aux rest
     | ((')' | ']' | '}' | '>') as c) :: rest ->
-      if Stack.is_empty s then Either.second c
-      else begin
-        let top = Stack.pop_exn s in
-        if Char.equal top c
-        then aux rest
-        else Either.second c
-      end
+      if Stack.is_empty s || not @@ Char.equal (Stack.pop_exn s) c then
+        Either.second c
+      else
+        aux rest
     | _ -> assert false
   in
   aux (String.to_list line)
@@ -43,13 +40,10 @@ let solve1 l = List.sum (module Int) ~f:score1 l
 
 let solve2 l =
   let res =
-    List.map l ~f:(fun s ->
-        Stack.fold ~init:0 ~f:(fun cpt c ->
-            cpt * 5 + score2 c
-          ) s
-      )
-    |> List.sort ~compare |> List.to_array
-  in res.(Array.length res / 2)
+    List.map ~f:(fun s ->
+        Stack.fold ~init:0 ~f:(fun acc c -> acc * 5 + score2 c) s
+      ) l |> List.sort ~compare
+  in List.nth_exn res (List.length res / 2)
 
 let main file =
   let data = Stdio.In_channel.read_lines file in
