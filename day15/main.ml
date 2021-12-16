9e44bae173917a84397ccc2bf0f3b87a32ae7397
Thibault Martin <twenty2bar@gmail.com>
2021-12-16 13:03:07 +0100

    Opti, custom graph, merci valoche

--- day15/main.ml
+++ day15/main.ml
@@ -1,79 +1,68 @@
 open Base
 open Graph
 
-module Node = struct
-  type t = int * int
-  let compare = Poly.compare
-  let hash = Hashtbl.hash
-  let equal = Poly.(=)
-  let default = 0
-end
-
-module Label = struct
-  include Int
-  let default = 0
-end
-
-module G = Imperative.Digraph.ConcreteLabeled(Node)(Label)
-
-module Weight = struct
-  type edge = G.E.t
-  type t = int
-  let weight = G.E.label
-  let compare = compare
-  let add = (+)
-  let zero = 0
-end
-
-module D = Path.Dijkstra(G)(Weight)
-
-let in_range a (x,y) =
-  y >= 0 && y < Array.length a && x >= 0 && x < Array.length a.(0)
-
-let find_neighbors a (x,y) =
-  let deltas = [(-1, 0); (0, -1); (1, 0); (0, 1)] in
-  let coords = List.map ~f:(fun (dx, dy) -> (x + dx, y + dy)) deltas in
-  List.filter ~f:(in_range a) coords
-
 let string_to_int_list s =
   String.to_list s |> List.map ~f:(Fn.compose Int.of_string Char.to_string)
 
-let inc_dedup l =
-  let rec aux acc last n =
-    if n = 5 then acc
-    else begin
-      let l = List.map last ~f:(fun i -> i % 9 + 1) in
-      aux (acc @ l) l (n+1)
+let parse_map data =
+  List.map data ~f:string_to_int_list
+  |> Array.of_list_map ~f:(Array.of_list_map ~f:Fn.id)
+
+let in_range (w,h) (x,y) =
+  y >= 0 && y < h && x >= 0 && x < w
+
+let find_neighbors g (x,y) =
+  let deltas = [(-1, 0); (0, -1); (1, 0); (0, 1)] in
+  let coords = List.map ~f:(fun (dx, dy) -> (x + dx, y + dy)) deltas in
+  List.filter ~f:(in_range g) coords
+
+let risk map (x,y) =
+  let mx,my = Array.length map.(0), Array.length map in
+  let r = map.(y % my).(x % mx) + (y / my + x / mx) in
+  if r <= 9 then r else r-9
+
+let solve ~big data =
+  let map = parse_map data in
+  let mapx, mapy = Array.length map.(0), Array.length map in
+  let mx,my = if big then mapx*5, mapy*5 else mapx, mapy in
+  let module G = struct
+    type t = int*int
+    module V = struct
+      type t = int * int
+      let compare = Poly.compare
+      let hash = Hashtbl.hash
+      let equal = Poly.(=)
     end
-  in aux l l 1
+    module E = struct
+      type t = V.t * V.t
+      type label = unit
+      let label _ = ()
+      let src (src,_) = src
+      let dst (_,dst) = dst
+      let create src _ dst = src,dst
+    end
+    let iter_vertex _ _ = ()
+    let fold_vertex _ _ a = a
+    let iter_succ _ _ _ = ()
+    let iter_succ_e f g src = List.iter ~f:(fun dst -> f (src,dst)) (find_neighbors g src)
+    let fold_edges_e _ _ a = a
+    let nb_vertex _ = 0
+  end
+  in
+  let module W = struct
+    type t = int
+    type edge = G.E.t
+    let weight (_,dst) = risk map dst
+    let compare = (-)
+    let add = (+)
+    let zero = 0
+  end
+  in
+  let module D = Path.Dijkstra(G)(W) in
+  snd @@ D.shortest_path (mx,my) (0,0) (mx-1,my-1)
 
-let fill_graph g map =
-  Array.iteri ~f:(fun y row ->
-      Array.iteri ~f:(fun x _ ->
-          List.iter ~f:(fun (x',y') ->
-              G.add_edge_e g (G.E.create (x,y) map.(y').(x') (x',y'))
-            ) (find_neighbors map (x,y))
-        ) row
-    ) map
-
-let parse_map_full data =
-  List.map data ~f:(fun row -> inc_dedup @@ string_to_int_list row)
-  |> List.transpose_exn |> List.map ~f:inc_dedup |> List.transpose_exn
-  |> Array.of_list_map ~f:(Array.of_list_map ~f:Fn.id)
-
-let parse_map_normal data =
-  List.map data ~f:(fun row -> string_to_int_list row)
-  |> Array.of_list_map ~f:(Array.of_list_map ~f:Fn.id)
-
-let solve f data =
-  let g = G.create () in
-  let map = f data in
-  fill_graph g map;
-  let max = Array.length map.(0) - 1, Array.length map - 1 in
-  snd @@ D.shortest_path g (0,0) max
-
-let solve1 = solve parse_map_normal
-let solve2 = solve parse_map_full
+let solve1 = solve ~big:false
+let solve2 = solve ~big:true
 
 let main file =
   let data = Stdio.In_channel.read_lines file in
